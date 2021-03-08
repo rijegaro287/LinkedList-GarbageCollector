4930a3251025d7b7779e48a608ede52df8652423
Ricardo Gatgens Rodríguez <gatgens48@gmail.com>
2021-03-07 19:47:39 -0600

    [update] Arreglos

--- GUI.cpp
+++ GUI.cpp
@@ -103,7 +103,7 @@ void MainWindow::addButtonClicked(wxCommandEvent & e) {
     }
 }
 
-void MainWindow::deleteButtonClicked(wxCommandEvent &e) {
+void MainWindow::deleteButtonClicked(wxCommandEvent & e) {
     if(entryDelete->GetValue() != "" && entryDelete->GetValue().IsNumber()) {
         list->deleteItem(wxAtoi(entryDelete->GetValue()));
         updateLists();
